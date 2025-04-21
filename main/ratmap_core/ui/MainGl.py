5dcb2d290ebaa4720431604e112c693719efd341
JehhB <jwy.bayod@gmail.com>
2025-04-21 15:14:59 +0800

    Added mesh for main gl

--- main/ratmap_core/ui/MainGl.py
+++ main/ratmap_core/ui/MainGl.py
@@ -4,9 +4,10 @@ import ctypes
 import math
 import time
 import tkinter as tk
-from typing import final
+from typing import List, final
 
 import numpy as np
+from numpy.typing import NDArray
 from OpenGL import GL
 from pyopengltk import OpenGLFrame
 from typing_extensions import override
@@ -14,11 +15,98 @@ from typing_extensions import override
 from ratmap_common.EventTarget import EventTarget
 
 
+class Mesh:
+    def __init__(
+        self,
+        vertices: NDArray[np.float32],
+        indices: NDArray[np.uint32],
+        pos_loc: int = 0,
+        color_loc: int = 1,
+    ):
+        self.__vao = GL.glGenVertexArrays(1)
+        self.__vbo = 0
+        self.__ebo = 0
+
+        self.__pos_loc = pos_loc
+        self.__color_loc = color_loc
+
+        self.__updated = True
+        self.__vertices = vertices
+        self.__indeces = indices
+
+    def __update_buffers(self):
+        if not self.__updated:
+            return
+
+        GL.glBindVertexArray(self.__vao)
+
+        if self.__vbo != 0:
+            GL.glDeleteBuffers(1, [self.__vbo])
+        if self.__ebo != 0:
+            GL.glDeleteBuffers(1, [self.__ebo])
+
+        self.__vbo = GL.glGenBuffers(1)
+        self.__ebo = GL.glGenBuffers(1)
+
+        GL.glBindBuffer(GL.GL_ARRAY_BUFFER, self.__vbo)
+        GL.glBufferData(
+            GL.GL_ARRAY_BUFFER,
+            self.__vertices.nbytes,
+            self.__vertices,
+            GL.GL_STATIC_DRAW,
+        )
+
+        GL.glBindBuffer(GL.GL_ELEMENT_ARRAY_BUFFER, self.__ebo)
+        GL.glBufferData(
+            GL.GL_ELEMENT_ARRAY_BUFFER,
+            self.__indeces.nbytes,
+            self.__indeces,
+            GL.GL_STATIC_DRAW,
+        )
+
+        stride = 6 * 4  # x,y,z,r,g,b (6 floats, 4 bytes each)
+
+        GL.glEnableVertexAttribArray(self.__pos_loc)
+        GL.glVertexAttribPointer(
+            self.__pos_loc, 3, GL.GL_FLOAT, False, stride, ctypes.c_void_p(0)
+        )
+
+        # Color: location 1
+        GL.glEnableVertexAttribArray(self.__color_loc)
+        GL.glVertexAttribPointer(
+            self.__color_loc, 3, GL.GL_FLOAT, False, stride, ctypes.c_void_p(12)
+        )
+
+        GL.glBindVertexArray(0)
+        GL.glBindBuffer(GL.GL_ARRAY_BUFFER, 0)
+
+        self.__updated = True
+
+    def draw(self):
+        self.__update_buffers()
+
+        GL.glBindVertexArray(self.__vao)
+        GL.glDrawElements(GL.GL_POINTS, 3, GL.GL_UNSIGNED_INT, ctypes.c_void_p(0))
+        GL.glBindVertexArray(0)
+
+    def free(self):
+        GL.glDeleteVertexArrays(1, [self.__vao])
+
+        if self.__vbo != 0:
+            GL.glDeleteBuffers(1, [self.__vbo])
+        self.__vbo = 0
+
+        if self.__ebo != 0:
+            GL.glDeleteBuffers(1, [self.__ebo])
+        self.__ebo = 0
+
+
 @final
 class MainGl(OpenGLFrame):
     DEFAULT_GL_WIDTH = 640
     DEFAULT_GL_HEIGHT = 480
     __event_target: EventTarget
+    __mesh: List[Mesh]
 
     def __init__(self, master: tk.Misc) -> None:
         super().__init__(
@@ -27,14 +115,15 @@ class MainGl(OpenGLFrame):
         self.__event_target = EventTarget()
         self.start_time = time.time()
         self.shader_program = None
-        self.vao = None
-        self.vbo = None
+
+        self.__mesh = []
 
     @override
     def initgl(self):
         # Set up viewport and clear color
         GL.glViewport(0, 0, self.width, self.height)
         GL.glClearColor(0.1, 0.1, 0.1, 1.0)
+        GL.glPointSize(10)
 
         # Create shaders and program
         self.create_shader_program()
@@ -143,22 +232,8 @@ class MainGl(OpenGLFrame):
             ],
             dtype=np.float32,
         )
-
-        # Create VBO
-        self.vbo = GL.glGenBuffers(1)
-
-        # Bind and set vertex buffer
-        GL.glBindBuffer(GL.GL_ARRAY_BUFFER, self.vbo)
-        GL.glBufferData(
-            GL.GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL.GL_STATIC_DRAW
-        )
-
-        # Get attribute locations
-        self.pos_attrib = GL.glGetAttribLocation(self.shader_program, "aPos")
-        self.color_attrib = GL.glGetAttribLocation(self.shader_program, "aColor")
-
-        # Unbind VBO
-        GL.glBindBuffer(GL.GL_ARRAY_BUFFER, 0)
+        indeces = np.array([0, 1, 2], dtype=np.uint32)
+        self.__mesh.append(Mesh(vertices, indeces))
 
     @override
     def redraw(self):
@@ -176,43 +251,8 @@ class MainGl(OpenGLFrame):
         rotation_location = GL.glGetUniformLocation(self.shader_program, "rotation")
         GL.glUniform1f(rotation_location, rotation_angle)
 
-        # Bind buffer and set attributes
-        GL.glBindBuffer(GL.GL_ARRAY_BUFFER, self.vbo)
-
-        # Position attribute
-        stride = 6 * 4  # 6 floats, 4 bytes each
-        offset = 0
-        GL.glEnableVertexAttribArray(self.pos_attrib)
-        GL.glVertexAttribPointer(
-            self.pos_attrib,
-            3,
-            GL.GL_FLOAT,
-            GL.GL_FALSE,
-            stride,
-            ctypes.c_void_p(offset),
-        )
-
-        # Color attribute
-        offset = 3 * 4  # Skip 3 floats, 4 bytes each
-        GL.glEnableVertexAttribArray(self.color_attrib)
-        GL.glVertexAttribPointer(
-            self.color_attrib,
-            3,
-            GL.GL_FLOAT,
-            GL.GL_FALSE,
-            stride,
-            ctypes.c_void_p(offset),
-        )
-
-        # Draw the triangle
-        GL.glDrawArrays(GL.GL_TRIANGLES, 0, 3)
-
-        # Disable attributes
-        GL.glDisableVertexAttribArray(self.pos_attrib)
-        GL.glDisableVertexAttribArray(self.color_attrib)
-
-        # Unbind buffer
-        GL.glBindBuffer(GL.GL_ARRAY_BUFFER, 0)
+        for mesh in self.__mesh:
+            mesh.draw()
 
         # Schedule the next redraw
         _ = self.after(16, self.tkExpose, None)  # ~60 FPS
@@ -226,8 +266,10 @@ class MainGl(OpenGLFrame):
         # Clean up OpenGL resources
         if self.shader_program:
             GL.glDeleteProgram(self.shader_program)
-        if self.vbo:
-            GL.glDeleteBuffers(1, [self.vbo])
+
+        for mesh in self.__mesh:
+            mesh.free()
+        self.__mesh = []
 
         self.__event_target.dispose()
         return super().destroy()
